1424b625bb66c2460da4f9c76802181cfea2fb78
allen <allen@allen-18.04>
2020-04-03 17:57:05 +0800

    up

---
+++ aarch64-depends-src/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/DependInfo.cmake
@@ -0,0 +1,107 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/any.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/any.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/any.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/any.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/api.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/api.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/arena.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/arena.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/arenastring.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/arenastring.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/compiler/importer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/compiler/importer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/compiler/parser.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/compiler/parser.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor_database.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/descriptor_database.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/duration.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/duration.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/dynamic_message.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/dynamic_message.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/empty.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/empty.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/extension_set.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/extension_set.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/extension_set_heavy.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/extension_set_heavy.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/field_mask.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/field_mask.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_reflection.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_reflection.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_table_driven.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_table_driven.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_table_driven_lite.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_table_driven_lite.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/generated_message_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/coded_stream.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/coded_stream.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/gzip_stream.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/gzip_stream.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/printer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/printer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/strtod.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/strtod.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/tokenizer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/tokenizer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream_impl.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream_impl.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream_impl_lite.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/io/zero_copy_stream_impl_lite.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/map_field.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/map_field.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/message.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/message.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/message_lite.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/message_lite.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/reflection_ops.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/reflection_ops.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/repeated_field.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/repeated_field.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/service.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/service.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/source_context.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/source_context.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/struct.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/struct.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/atomicops_internals_x86_msvc.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/atomicops_internals_x86_msvc.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/bytestream.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/bytestream.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/common.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/common.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/int128.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/int128.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/io_win32.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/io_win32.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/mathlimits.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/mathlimits.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/once.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/once.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/status.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/status.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/statusor.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/statusor.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/stringpiece.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/stringpiece.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/stringprintf.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/stringprintf.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/structurally_valid.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/structurally_valid.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/strutil.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/strutil.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/substitute.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/substitute.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/time.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/stubs/time.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/text_format.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/text_format.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/timestamp.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/timestamp.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/type.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/type.pb.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/unknown_field_set.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/unknown_field_set.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/delimited_message_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/delimited_message_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/field_comparator.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/field_comparator.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/field_mask_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/field_mask_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/datapiece.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/datapiece.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/default_value_objectwriter.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/default_value_objectwriter.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/error_listener.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/error_listener.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/field_mask_utility.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/field_mask_utility.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_escaping.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_escaping.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_objectwriter.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_objectwriter.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_stream_parser.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/json_stream_parser.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/object_writer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/object_writer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/proto_writer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/proto_writer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/protostream_objectsource.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/protostream_objectsource.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/protostream_objectwriter.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/protostream_objectwriter.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/type_info.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/type_info.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/type_info_test_helper.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/type_info_test_helper.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/utility.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/internal/utility.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/json_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/json_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/message_differencer.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/message_differencer.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/time_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/time_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/util/type_resolver_util.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/util/type_resolver_util.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/wire_format.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/wire_format.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/wire_format_lite.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/wire_format_lite.cc.o"
+  "/home/allen/protobuf-3.5.1/src/google/protobuf/wrappers.pb.cc" "/home/allen/protobuf-3.5.1/cmake/build/CMakeFiles/libprotobuf.dir/home/allen/protobuf-3.5.1/src/google/protobuf/wrappers.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GOOGLE_PROTOBUF_CMAKE_BUILD"
+  "HAVE_PTHREAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/allen/protobuf-3.5.1/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aarch64-depends-src/protobuf-3.6.1/cmake/build/CMakeFiles/protoc.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/allen/protobuf-3.6.1/src/google/protobuf/compiler/main.cc" "/home/allen/protobuf-3.6.1/cmake/build/CMakeFiles/protoc.dir/home/allen/protobuf-3.6.1/src/google/protobuf/compiler/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GOOGLE_PROTOBUF_CMAKE_BUILD"
+  "HAVE_PTHREAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/allen/protobuf-3.6.1/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/allen/protobuf-3.6.1/cmake/build/CMakeFiles/libprotoc.dir/DependInfo.cmake"
+  "/home/allen/protobuf-3.6.1/cmake/build/CMakeFiles/libprotobuf.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
